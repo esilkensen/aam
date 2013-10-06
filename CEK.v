7a02d3469285c005921390752842cc1f93caabb4
Erik Silkensen <silkense@colorado.edu>
2013-10-05 21:11:36 -0400

    CEK version 1

--- CEK.v
+++ CEK.v
@@ -1,81 +1,87 @@
-(* CEK: The CEK machine *)
-
 Require Export SfLib.
 
-(* ###################################################################### *)
+Module CEK.
 
 Inductive expr : Type :=
-| e_var : id -> expr
-| e_app : expr -> expr -> expr
-| e_abs : id -> expr -> expr.
-
-Tactic Notation "e_cases" tactic(first) ident(c) :=
-  first;
-  [ Case_aux c "e_var" | Case_aux c "e_app" | Case_aux c "e_abs" ].
+  | var : id -> expr
+  | app : expr -> expr -> expr
+  | abs : id -> expr -> expr.
 
 Inductive val : Type :=
-| v_clo : id -> expr -> (partial_map val) -> val.
+  | clo : id -> expr -> partial_map val -> val.
 
-Notation env := (partial_map val).
+Definition env := partial_map val.
 
 Inductive frame : Type :=
-| app_r : expr -> frame
-| app_l : val -> frame.
+  | ar : expr -> frame
+  | fn : val -> frame.
 
-Notation kont := (list frame).
+Definition kont := list frame.
 
 Inductive state : Type :=
-| s_eval : expr -> env -> kont -> state
-| s_apply : val -> kont -> state.
+  | ev : expr -> env -> kont -> state
+  | ap : val -> kont -> state.
+
+Definition inj : expr -> state :=
+  fun e => ev e empty [].
+
+Reserved Notation "s1 '==>' s2" (at level 40).
 
 Inductive step : state -> state -> Prop :=
-| CEK_var :
-    forall x p k v,
-      p x = Some v ->
-      step (s_eval (e_var x) p k)
-           (s_apply v k)
-| CEK_abs :
-    forall x e p k,
-      step (s_eval (e_abs x e) p k)
-           (s_apply (v_clo x e p) k)
-| CEK_app1 :
-    forall e1 e2 p k,
-      step (s_eval (e_app e1 e2) p k)
-           (s_eval e1 p (app_r e2 :: k))
-| CEK_app2 :
-    forall x e p e2 k,
-      step (s_apply (v_clo x e p) (app_r e2 :: k))
-           (s_eval e2 p (app_l (v_clo x e p) :: k))
-| CEK_app3 :
-    forall x e p v2 k,
-      step (s_apply v2 (app_l (v_clo x e p) :: k))
-           (s_eval e (extend p x v2) k).
+  | cek1 :
+      forall x p k v,
+        p x = Some v ->
+        ev (var x) p k
+        ==>
+        ap v k
+  | cek2 :
+      forall x e p k,
+        ev (abs x e) p k
+        ==>
+        ap (clo x e p) k
+  | cek3 :
+      forall e1 e2 p k,
+        ev (app e1 e2) p k
+        ==>
+        ev e1 p (ar e2 :: k)
+  | cek4 :
+      forall x e p e2 k,
+        ap (clo x e p) (ar e2 :: k)
+        ==>
+        ev e2 p (fn (clo x e p) :: k)
+  | cek5 :
+      forall x e p v2 k,
+        ap v2 (fn (clo x e p) :: k)
+        ==>
+        ev e (extend p x v2) k
+
+where "s1 '==>' s2" := (step s1 s2).
+
+Tactic Notation "expr_cases" tactic(first) ident(c) :=
+  first;
+  [ Case_aux c "var" | Case_aux c "app" | Case_aux c "abs" ].
 
 Tactic Notation "step_cases" tactic(first) ident(c) :=
   first;
-  [ Case_aux c "CEK_var" | Case_aux c "CEK_abs" | Case_aux c "CEK_app1"
-    | Case_aux c "CEK_app2" | Case_aux c "CEK_app3" ].
+  [ Case_aux c "cek1" | Case_aux c "cek2" | Case_aux c "cek3"
+    | Case_aux c "cek4" | Case_aux c "cek5" ].
 
 Hint Constructors step.
 
-Notation "t1 '==>' t2" := (step t1 t2) (at level 40).
-
 Notation "t1 '==>*' t2" := (multi step t1 t2) (at level 40).
 
-(* Examples *)
-
-Example example1 :
+Example ex1 :
   forall x y,
-    (s_eval (e_app (e_abs x (e_var x)) (e_abs y (e_var y))) empty [])
-      ==>*
-    (s_apply (v_clo y (e_var y) empty) []).
+    inj (app (abs x (var x)) (abs y (var y))) ==>* ap (clo y (var y) empty) [].
 Proof.
-  intros.
+  intros. unfold inj.
   eapply multi_step. auto.
   eapply multi_step. auto.
   eapply multi_step. auto.
   eapply multi_step. auto.
   eapply multi_step. auto.
-  eapply multi_step. apply CEK_var. apply extend_eq.
+  eapply multi_step. apply cek1. apply extend_eq.
   apply multi_refl.
 Qed.
+
+End CEK.
